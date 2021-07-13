23163d7173daff8f8c7c24d2d68a36c0390a5006
lbfico <87351247+lbfico@users.noreply.github.com>
2021-07-13 19:23:29 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+makeCacheMatrix <- function(x=matrix(),...){
+       m<-NULL
+       set<-function(y){
+               x << - y # with << - can be the starting assignment behavior were carried out on the environment hierarchy
+               m<<-NULL
+       }
+       get<-function() x
+       setM<-function(solve) m<<-solve
+       getM<-function() m
+       list(set=set,get=get,setM=setM,getM=getM)
 }
